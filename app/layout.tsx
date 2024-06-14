789512c0969ce2ebec36008dc6b3d98901976e51
Man <manishgodhani.tp@gmail.com>
2024-06-15 02:30:41 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import { IBM_Plex_Sans } from "next/font/google";
+
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const IBMPlex = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ['400','500','600','700'],
+  variable: '--font-ibm-plex'
+});
+
+export const metadata: Metadata = {
+  title: "Imaginify",
+  description: "AI-powered Image Generation",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider appearance={{variables: {colorPrimary: '#624cf5'}}}>
+    <html lang="en">
+      <body className={cn("font-IBMPlex antialiased",IBMPlex.variable)}>
+         {/* <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn> */}
+        {children}
+        </body>
+    </html>
+    </ClerkProvider>
+  );
+}
